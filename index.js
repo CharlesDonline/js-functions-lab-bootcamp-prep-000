d3ca8918f836c208fecc9cffebd2016499af0a2c
Charlton Doucette <charlesdonline@gmail.com>
2018-04-05 15:52:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// write your code below!
+}
+function happyHolidays(string) {
+  return "Happy holidays!" 
+}
+function happyHolidaysTo(string) {
+  else if 'Amy' ; return 'Happy holidays, ${name}!'
+}
+function happyHolidayTo(string) {
+  else if 'Christmas'
+  else if 'Sarah'
+  return 'Happy @{holiday}, ${name}!'
+}
