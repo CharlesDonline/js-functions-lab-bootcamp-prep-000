f63b4bd9837a4553547fd2d973198eef9040dd36
Charlton Doucette <charlesdonline@gmail.com>
2018-04-05 16:05:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function happyHolidays(string) {
   return "Happy holidays!"
 }
 function happyHolidaysTo(name) {
-  return "Happy holidays, ${name}!"
+  return "Happy holidays, ${name}!" 
 }
 function happyHolidaysTo(holiday, name) {
   return "Happy ${holiday}, ${name}!"
